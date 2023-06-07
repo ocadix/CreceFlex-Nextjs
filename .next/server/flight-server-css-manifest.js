1db093996de6e178835861cbd4d1ddaee42daab5
¨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¨ <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>
2023-06-07 15:06:50 -0500

    Base Nextjs CreceFlex

---
+++ .next/server/flight-server-css-manifest.js
@@ -0,0 +1,14 @@
+self.__RSC_CSS_MANIFEST={
+  "cssImports": {
+    "/home/panditadev/Proyectos/CreceFlex-Nextjs/src/app/layout.tsx": [
+      "/home/panditadev/Proyectos/CreceFlex-Nextjs/node_modules/next/font/google/target.css?{\"path\":\"src/app/layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}",
+      "/home/panditadev/Proyectos/CreceFlex-Nextjs/src/app/globals.css"
+    ]
+  },
+  "cssModules": {
+    "/home/panditadev/Proyectos/CreceFlex-Nextjs/src/app/page": [
+      "/home/panditadev/Proyectos/CreceFlex-Nextjs/src/app/globals.css",
+      "/home/panditadev/Proyectos/CreceFlex-Nextjs/node_modules/next/font/google/target.css?{\"path\":\"src/app/layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}"
+    ]
+  }
+}
